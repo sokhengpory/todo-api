6d711bee9316f95410ebfe3e91f10bd28b54fdb2
Sokheng <porysokheng@gmail.com>
2022-06-28 17:14:27 +0700

    setup basic CRUD

--- src/routes/taskRoute.js
+++ src/routes/taskRoute.js
@@ -1,11 +1,58 @@
 const express = require('express');
+const Task = require('../models/task');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.status(200).send({
-    message: 'success',
-  });
+  try {
+    res.status(200).send({
+      message: 'success',
+    });
+  } catch (error) {
+    res.status(404).send({
+      message: 'fail',
+      error,
+    });
+  }
+});
+
+router.post('/', (req, res) => {
+  try {
+    res.status(201).send({
+      message: 'success',
+    });
+  } catch (error) {
+    res.status(400).send({
+      message: 'fail',
+      error,
+    });
+  }
+});
+
+router.patch('/:id', (req, res) => {
+  try {
+    res.status(200).send({
+      message: 'success',
+    });
+  } catch (error) {
+    res.status(404).send({
+      message: 'fail',
+      error,
+    });
+  }
+});
+
+router.delete('/:id', (req, res) => {
+  try {
+    res.status(200).send({
+      message: 'success',
+    });
+  } catch (error) {
+    res.status(404).send({
+      message: 'fail',
+      error,
+    });
+  }
 });
 
 module.exports = router;
