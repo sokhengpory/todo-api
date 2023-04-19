6f7dff934b899e5348b22cb9ce267ca348118204
Sokheng <porysokheng@gmail.com>
2023-04-19 11:05:50 +0700

    use Intl DateTimeFormat instead of normal Date

--- src/index.js
+++ src/index.js
@@ -1,27 +1,19 @@
 const express = require('express');
 const cors = require('cors');
-const morgan = require('morgan');
 
 require('dotenv').config();
 require('./db/mongoose');
 
 const taskRoute = require('./routes/taskRoute');
 const userRoute = require('./routes/userRoute');
+const logger = require('./middleware/logger');
 
 const app = express();
-
 const PORT = process.env.PORT || 5000;
-morgan.token('date', () => {
-  return new Date().toLocaleString();
-});
 
 app.use(express.json());
 app.use(cors());
-app.use(
-  morgan(
-    '[:remote-addr] [:method - :url] [:date] [:status - :response-time ms]'
-  )
-);
+app.use(logger());
 
 app.get('/test', (req, res) => {
   return res.send({
---
+++ src/middleware/logger.js
@@ -0,0 +1,15 @@
+const morgan = require('morgan');
+
+morgan.token('date', () => {
+  const dateTimeFormat = new Intl.DateTimeFormat('en-US', {
+    dateStyle: 'medium',
+    timeStyle: 'medium',
+    timeZone: 'Asia/Bangkok',
+  });
+  return dateTimeFormat.format(new Date());
+});
+
+const loggerFormat =
+  '[:remote-addr] [:method - :url] [:date] [:status - :response-time ms]';
+
+module.exports = () => morgan(loggerFormat);
